5c26507f27acde82c4c7209a18219f4ccf52143c
Moritz Eysholdt <moritz.eysholdt@typefox.io>
2019-01-22 13:40:58 +0000

    Added another article

--- models.article.go
+++ models.article.go
@@ -17,6 +17,7 @@ var articleList = []article{
 	article{ID: 1, Title: "Article 1", Content: "Article 1 body"},
 	article{ID: 2, Title: "Article 2", Content: "Article 2 body"},
     article{ID: 2, Title: "Article 3", Content: "Article 3 body"},
+    article{ID: 2, Title: "Article 4", Content: "Article 4 body"},
 }
 
 // Return a list of all the articles
